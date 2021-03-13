9c3be926da09ab9cb8c3b4fe673952a71d7ac38e
vitalikulsha <kulsha_85@mail.ru>
2021-03-13 20:52:08 +0300

    delete Main.java and HolderTicket.java

--- src/main/java/io/github/vitalikulsha/telegrambot/Main.java
+++
@@ -1,9 +0,0 @@
-package io.github.vitalikulsha.telegrambot;
-
-import io.github.vitalikulsha.telegrambot.util.HolderTicket;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(new HolderTicket().toString());
-    }
-}
