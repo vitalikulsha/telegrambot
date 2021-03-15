c2638ce446403121b76017bf5cced8e9bb35d5ad
Vitali Kulsha <73823615+vitalikulsha@users.noreply.github.com>
2021-03-15 08:46:29 +0300

    initial commit

---
+++ src/main/java/io/github/vitalikulsha/telegrambot/util/DatabaseAdmin.java
@@ -0,0 +1,10 @@
+package io.github.vitalikulsha.telegrambot.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class TextMessageUtil {
+protected static final String PASSWORD = "adminpassword";
+  
+}
