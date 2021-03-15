91cf5cb90d1829bcddf270d5b2ac27285a7a7cc1
Vitali Kulsha <73823615+vitalikulsha@users.noreply.github.com>
2021-03-15 09:32:59 +0300

    add tripReserveSet

--- src/main/java/io/github/vitalikulsha/telegrambot/util/DatabaseAdmin.java
+++ src/main/java/io/github/vitalikulsha/telegrambot/util/DatabaseAdmin.java
@@ -4,7 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-public class TextMessageUtil {
-protected static final String PASSWORD = "adminpassword";
+public class DatabaseAdmin {
+  protected static final String PASSWORD = "adminpassword";
   
+  public static Set<String> tripReserveSet = new HashSet<>() {
+        {
+            this.add("МнМг0803211020");
+            this.add("МнМг0803211230");
+            this.add("МнМг0803211440");
+            this.add("МнМг0803211650");
+            this.add("МнМг0903211020");
+            this.add("МнМг0903211230");
+            this.add("МнМг0903211440");
+            this.add("МнМг0903211650");
+            this.add("МнМг1003211020");
+            this.add("МнМг1003211230");
+            this.add("МнМг1003211440");
+            this.add("МнМг1003211650");
+            this.add("МнМг1003211020");
+            this.add("МнМг1103211230");
+            this.add("МнМг1103211440");
+            this.add("МнМг1103211650");
+            this.add("МгМн0803210810");
+            this.add("МгМн0803211115");
+            this.add("МгМн0803211310");
+            this.add("МгМн0803211840");
+            this.add("МгМн0903210810");
+            this.add("МгМн0903211115");
+            this.add("МгМн0903211310");
+            this.add("МгМн0903211840");
+            this.add("МгМн1003210810");
+            this.add("МгМн1003211115");
+            this.add("МгМн1003211310");
+            this.add("МгМн1003211840");
+            this.add("МгМн1103210810");
+            this.add("МгМн1103211115");
+            this.add("МгМн1103211310");
+            this.add("МгМн1103211840");          
+        }
+    };
 }
